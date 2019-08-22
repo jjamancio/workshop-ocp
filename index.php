e4bf31b924ad5b7b9b382c0de236cdfed733f17e
jjamancio <jjamancio@gmail.com>
2019-08-22 11:37:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo "<h1>Openshift Workshop v1.0</h1> ";
-Print "<strong>"$_SERVER['SERVER_ADDR']"</strong>";
+Print "<strong>Endereço IP do POD: ".$_SERVER['SERVER_ADDR']."</strong><br>";
 ?>
